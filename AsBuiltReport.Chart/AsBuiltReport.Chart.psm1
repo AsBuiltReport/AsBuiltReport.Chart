70af96408db1cfd6f5b5e557f0478ab195c59b19
Jonathan Colon <rebelinux@gmail.com>
2026-02-20 20:19:45 -0400

    Enhance module import statements for macOS ARM and x86 architectures with verbose and debug output

--- AsBuiltReport.Chart/AsBuiltReport.Chart.psm1
+++ AsBuiltReport.Chart/AsBuiltReport.Chart.psm1
@@ -5,15 +5,15 @@ switch ($PSVersionTable.PSEdition) {
             $architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
             if ($architecture -eq "Arm64" -or $architecture -eq "Arm") {
                 Write-Verbose "Architecture: ARM (Apple Silicon)"
-                Import-Module ("$PSScriptRoot{0}Src{0}Assemblies{0}Core{0}mac-osx{0}arm{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
+                Import-Module ("$PSScriptRoot{0}Src{0}Assemblies{0}Core{0}mac-osx{0}arm{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar) -Verbose -Debug
 
             } elseif ($architecture -eq "X64" -or $architecture -eq "X86") {
                 Write-Verbose "Architecture: x86 (Intel)"
-                Import-Module ("$PSScriptRoot{0}Src{0}Assemblies{0}Core{0}mac-osx{0}x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
+                Import-Module ("$PSScriptRoot{0}Src{0}Assemblies{0}Core{0}mac-osx{0}x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar) -Verbose -Debug
 
             } else {
                 Write-Verbose "Architecture: Unknown or other architecture"
-                Import-Module ("$PSScriptRoot{0}Src{0}Assemblies{0}Core{0}mac-osx{0}arm{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
+                Import-Module ("$PSScriptRoot{0}Src{0}Assemblies{0}Core{0}mac-osx{0}arm{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)  -Verbose -Debug
             }
         } elseif ($IsLinux) {
             Import-Module ("$PSScriptRoot{0}Src{0}Assemblies{0}Core{0}linux-x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
