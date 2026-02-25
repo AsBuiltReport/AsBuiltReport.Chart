afb95900e48a276542346f816d8592929855da5c
Jonathan Colon <rebelinux@gmail.com>
2026-02-25 16:05:04 -0400

    Refactor StackedBarChart to improve error handling and streamline color palette logic

--- Sources/StackedBarChart.cs
+++ Sources/StackedBarChart.cs
@@ -10,84 +10,78 @@ namespace AsBuiltReportChart
         static StackedBar() { }
         public object Chart(List<double[]> values, string[] labels, string[] categoryNames, string filename = "output", int width = 400, int height = 300)
         {
-             if (values[0].Length != categoryNames.Length)
+            Plot myPlot = new Plot();
+
+            if (EnableCustomColorPalette)
             {
-                throw new Exception("Error: Values and category names must be equal.");
+                if (_customColorPalette != null && _customColorPalette.Length > 0)
+                {
+                    myPlot.Add.Palette = colorPalette = new ScottPlot.Palettes.Custom(_customColorPalette);
+                }
+                else
+                {
+                    throw new Exception("CustomColorPalette is empty. Please provide valid color values.");
+                }
             }
-            if (values.Count == labels.Length)
+            else
             {
-                Plot myPlot = new Plot();
+                // Set ScottPlot native color palette
+                if (colorPalette != null)
+                {
+                    myPlot.Add.Palette = colorPalette;
+                }
+            }
 
-                if (EnableCustomColorPalette)
-                {
-                    if (_customColorPalette != null && _customColorPalette.Length > 0)
-                    {
-                        myPlot.Add.Palette = colorPalette = new ScottPlot.Palettes.Custom(_customColorPalette);
-                    }
-                    else
-                    {
-                        throw new Exception("CustomColorPalette is empty. Please provide valid color values.");
-                    }
-                }
-                else
-                {
-                    // Set ScottPlot native color palette
-                    if (colorPalette != null)
-                    {
-                        myPlot.Add.Palette = colorPalette;
-                    }
-                }
+            // Set X and Y axis label settings
+            if (AreaOrientation == Orientations.Horizontal)
+            {
+                myPlot.Axes.Bottom.Label.Text = LabelYAxis;
+            }
+            else if (AreaOrientation == Orientations.Vertical)
+            {
+                myPlot.Axes.Bottom.Label.Text = LabelXAxis;
+            }
+            else
+            {
+                myPlot.Axes.Bottom.Label.Text = "";
+            }
+            myPlot.Axes.Bottom.Label.FontSize = LabelFontSize;
+            myPlot.Axes.Bottom.Label.ForeColor = GetDrawingColor(LabelFontColor);
+            myPlot.Axes.Bottom.Label.FontName = FontName;
 
-                // Set X and Y axis label settings
-                if (AreaOrientation == Orientations.Horizontal)
-                {
-                    myPlot.Axes.Bottom.Label.Text = LabelYAxis;
-                }
-                else if (AreaOrientation == Orientations.Vertical)
-                {
-                    myPlot.Axes.Bottom.Label.Text = LabelXAxis;
-                }
-                else
-                {
-                    myPlot.Axes.Bottom.Label.Text = "";
-                }
-                myPlot.Axes.Bottom.Label.FontSize = LabelFontSize;
-                myPlot.Axes.Bottom.Label.ForeColor = GetDrawingColor(LabelFontColor);
-                myPlot.Axes.Bottom.Label.FontName = FontName;
+            myPlot.Axes.Bottom.TickLabelStyle.FontSize = LabelFontSize;
+            myPlot.Axes.Bottom.TickLabelStyle.ForeColor = GetDrawingColor(LabelFontColor);
+            myPlot.Axes.Bottom.TickLabelStyle.FontName = FontName;
+            myPlot.Axes.Bottom.Label.Bold = LabelBold;
 
-                myPlot.Axes.Bottom.TickLabelStyle.FontSize = LabelFontSize;
-                myPlot.Axes.Bottom.TickLabelStyle.ForeColor = GetDrawingColor(LabelFontColor);
-                myPlot.Axes.Bottom.TickLabelStyle.FontName = FontName;
-                myPlot.Axes.Bottom.Label.Bold = LabelBold;
+            // myPlot.Axes.Bottom.TickLabelStyle.Rotation = -10;
 
-                // myPlot.Axes.Bottom.TickLabelStyle.Rotation = -10;
+            if (AreaOrientation == Orientations.Horizontal)
+            {
+                myPlot.Axes.Left.Label.Text = LabelXAxis;
+            }
+            else if (AreaOrientation == Orientations.Vertical)
+            {
+                myPlot.Axes.Left.Label.Text = LabelYAxis;
+            }
+            else
+            {
+                myPlot.Axes.Left.Label.Text = "";
+            }
+            myPlot.Axes.Left.Label.FontSize = LabelFontSize;
+            myPlot.Axes.Left.Label.ForeColor = GetDrawingColor(LabelFontColor);
+            myPlot.Axes.Left.Label.FontName = FontName;
+            myPlot.Axes.Left.Label.Bold = LabelBold;
 
-                if (AreaOrientation == Orientations.Horizontal)
-                {
-                    myPlot.Axes.Left.Label.Text = LabelXAxis;
-                }
-                else if (AreaOrientation == Orientations.Vertical)
-                {
-                    myPlot.Axes.Left.Label.Text = LabelYAxis;
-                }
-                else
-                {
-                    myPlot.Axes.Left.Label.Text = "";
-                }
-                myPlot.Axes.Left.Label.FontSize = LabelFontSize;
-                myPlot.Axes.Left.Label.ForeColor = GetDrawingColor(LabelFontColor);
-                myPlot.Axes.Left.Label.FontName = FontName;
-                myPlot.Axes.Left.Label.Bold = LabelBold;
+            myPlot.Axes.Left.TickLabelStyle.FontSize = LabelFontSize;
+            myPlot.Axes.Left.TickLabelStyle.ForeColor = GetDrawingColor(LabelFontColor);
+            myPlot.Axes.Left.TickLabelStyle.FontName = FontName;
 
-                myPlot.Axes.Left.TickLabelStyle.FontSize = LabelFontSize;
-                myPlot.Axes.Left.TickLabelStyle.ForeColor = GetDrawingColor(LabelFontColor);
-                myPlot.Axes.Left.TickLabelStyle.FontName = FontName;
-
-                // create bars
-                var bars = new List<ScottPlot.Bar>();
-                // Set a moderate bar width when there is only one element to prevent it from filling the entire chart area
-                double barSize = values.Count == 1 ? 0.5 : 0.8;
-                // assign values and colors to each bar
+            // create bars
+            var bars = new List<ScottPlot.Bar>();
+            // assign values and colors to each bar
+            if (values.Count > 0 && values[0].Length > 1)
+            {
                 for (int x = 0; x < values.Count; x++)
                 {
                     double nextBarBase = 0;
@@ -103,140 +97,158 @@ namespace AsBuiltReportChart
                                 FillColor = colorPalette.GetColor(i),
                                 Label = $"{values[x][i]}",
                                 CenterLabel = true,
-                                Size = barSize,
                             });
                             nextBarBase += values[x][i];
                         }
                     }
                 }
-
-                // add bars to plot
-                var bar = myPlot.Add.Bars(bars);
-
-                // Customize bars label style, including color
-                bar.ValueLabelStyle.FontName = FontName;
-                bar.ValueLabelStyle.ForeColor = GetDrawingColor(LabelFontColor);
-                bar.ValueLabelStyle.Bold = LabelBold;
-                bar.ValueLabelStyle.FontSize = LabelFontSize;
-
-                // set each slice value to its label
-                ScottPlot.TickGenerators.NumericManual tickGen = new ScottPlot.TickGenerators.NumericManual();
-
-                // assign labels to each bar
-                if (AreaOrientation == Orientations.Vertical)
-                {
-                    for (var i = 0; i < categoryNames.Length; i++)
-                    {
-                        if (colorPalette != null && EnableLegend)
-                        {
-                            myPlot.Legend.ManualItems.Add(new LegendItem()
-                            {
-                                LabelText = categoryNames[i],
-                                FillColor = colorPalette.GetColor(i)
-                            });
-                        }
-                    }
-
-                    for (var i = 0; i < labels.Length; i++)
-                    {
-                        // set ticks
-                        tickGen.AddMajor(i, labels[i]);
-                    }
-                    myPlot.Axes.Bottom.TickGenerator = tickGen;
-                }
-                else
-                {
-                    for (var i = 0; i < categoryNames.Length; i++)
-                    {
-                        if (colorPalette != null && EnableLegend)
-                        {
-                            myPlot.Legend.ManualItems.Add(new LegendItem()
-                            {
-                                LabelText = categoryNames[i],
-                                FillColor = colorPalette.GetColor(i)
-                            });
-                        }
-                    }
-
-                    for (var i = 0; i < labels.Length; i++)
-                    {
-                        // set ticks
-                        tickGen.AddMajor(i, labels[i]);
-                    }
-                    // set ticks for horizontal orientation
-                    myPlot.Axes.Left.TickGenerator = tickGen;
-                }
-
-                bar.Horizontal = (AreaOrientation == Orientations.Horizontal);
-
-
-                // hide unnecessary plot components
-                myPlot.HideGrid();
-                myPlot.Axes.Top.IsVisible = false;
-                myPlot.Axes.Right.IsVisible = false;
-
-                if (EnableLegend)
-                {
-                    myPlot.ShowLegend();
-
-                    // Legend Font Properties
-                    myPlot.Legend.FontName = FontName;
-                    myPlot.Legend.FontSize = LegendFontSize;
-                    myPlot.Legend.FontColor = GetDrawingColor(LegendFontColor);
-
-                    // Legend box Style Properties
-                    myPlot.Legend.OutlineColor = GetDrawingColor(LegendBorderColor);
-                    myPlot.Legend.OutlineWidth = LegendBorderSize;
-
-                    myPlot.Legend.OutlinePattern = LegendBorderStyleMap[LegendBorderStyle];
-
-                    myPlot.Legend.Orientation = LegendOrientationMap[LegendOrientation];
-
-                    myPlot.Legend.Alignment = LegendAlignmentMap[LegendAlignment];
-                }
-
-                if (EnableChartBorder)
-                {
-                    myPlot.FigureBorder = new LineStyle()
-                    {
-                        // Set chart border properties
-                        Color = GetDrawingColor(ChartBorderColor),
-                        Width = ChartBorderSize,
-                        Pattern = ChartBorderStyleMap[ChartBorderStyle],
-                    };
-                }
-
-                // Set title properties
-                if (Title != null)
-                {
-                    myPlot.Title(Title);
-                    myPlot.Axes.Title.Label.FontSize = TitleFontSize;
-                    myPlot.Axes.Title.Label.ForeColor = GetDrawingColor(TitleFontColor);
-                    myPlot.Axes.Title.Label.Bold = TitleFontBold;
-                    myPlot.Axes.Title.Label.FontName = FontName;
-                }
-
-                // Set margins settings
-                if (AreaOrientation == Orientations.Horizontal)
-                {
-                    myPlot.Axes.Margins(left: AxesMarginsLeft, right: AxesMarginsRight, bottom: AxesMarginsDown, top: AxesMarginsTop);
-                }
-                else
-                {
-                    myPlot.Axes.Margins(left: AxesMarginsLeft, right: AxesMarginsRight, bottom: AxesMarginsDown, top: AxesMarginsTop);
-                }
-
-                // Set filepath
-
-                string Filepath = _outputFolderPath ?? Directory.GetCurrentDirectory();
-
-                // Set filename
-                return SaveInFormat(myPlot, width, height, Filepath, filename, Format);
             }
             else
             {
-                throw new ArgumentException("Error: Values and labels must be equal.");
+                // Set a moderate bar width when there is only one element to prevent it from filling the entire chart area
+                // double barSize = values.Count == 1 ? 5.0 : 8.0;
+                double nextBarBase = 0;
+                for (int x = 0; x < values.Count; x++)
+                {
+                    if (colorPalette != null)
+                    {
+                        bars.Add(new ScottPlot.Bar
+                        {
+                            Position = 0,
+                            Value = nextBarBase + values[x][0],
+                            ValueBase = nextBarBase,
+                            FillColor = colorPalette.GetColor(x),
+                            Label = $"{values[x][0]}",
+                            CenterLabel = true,
+                            // Size = barSize,
+                        });
+                        nextBarBase += values[x][0];
+                    }
+                }
             }
+
+            // add bars to plot
+            var bar = myPlot.Add.Bars(bars);
+
+            // Customize bars label style, including color
+            bar.ValueLabelStyle.FontName = FontName;
+            bar.ValueLabelStyle.ForeColor = GetDrawingColor(LabelFontColor);
+            bar.ValueLabelStyle.Bold = LabelBold;
+            bar.ValueLabelStyle.FontSize = LabelFontSize;
+
+            // set each slice value to its label
+            ScottPlot.TickGenerators.NumericManual tickGen = new ScottPlot.TickGenerators.NumericManual();
+
+            // assign labels to each bar
+            if (AreaOrientation == Orientations.Vertical)
+            {
+                for (var i = 0; i < categoryNames.Length; i++)
+                {
+                    if (colorPalette != null && EnableLegend)
+                    {
+                        myPlot.Legend.ManualItems.Add(new LegendItem()
+                        {
+                            LabelText = categoryNames[i],
+                            FillColor = colorPalette.GetColor(i)
+                        });
+                    }
+                }
+
+                for (var i = 0; i < labels.Length; i++)
+                {
+                    // set ticks
+                    tickGen.AddMajor(i, labels[i]);
+                }
+                myPlot.Axes.Bottom.TickGenerator = tickGen;
+            }
+            else
+            {
+                for (var i = 0; i < categoryNames.Length; i++)
+                {
+                    if (colorPalette != null && EnableLegend)
+                    {
+                        myPlot.Legend.ManualItems.Add(new LegendItem()
+                        {
+                            LabelText = categoryNames[i],
+                            FillColor = colorPalette.GetColor(i)
+                        });
+                    }
+                }
+
+                for (var i = 0; i < labels.Length; i++)
+                {
+                    // set ticks
+                    tickGen.AddMajor(i, labels[i]);
+                }
+                // set ticks for horizontal orientation
+                myPlot.Axes.Left.TickGenerator = tickGen;
+            }
+
+            bar.Horizontal = (AreaOrientation == Orientations.Horizontal);
+
+
+            // hide unnecessary plot components
+            myPlot.HideGrid();
+            myPlot.Axes.Top.IsVisible = false;
+            myPlot.Axes.Right.IsVisible = false;
+
+            if (EnableLegend)
+            {
+                myPlot.ShowLegend();
+
+                // Legend Font Properties
+                myPlot.Legend.FontName = FontName;
+                myPlot.Legend.FontSize = LegendFontSize;
+                myPlot.Legend.FontColor = GetDrawingColor(LegendFontColor);
+
+                // Legend box Style Properties
+                myPlot.Legend.OutlineColor = GetDrawingColor(LegendBorderColor);
+                myPlot.Legend.OutlineWidth = LegendBorderSize;
+
+                myPlot.Legend.OutlinePattern = LegendBorderStyleMap[LegendBorderStyle];
+
+                myPlot.Legend.Orientation = LegendOrientationMap[LegendOrientation];
+
+                myPlot.Legend.Alignment = LegendAlignmentMap[LegendAlignment];
+            }
+
+            if (EnableChartBorder)
+            {
+                myPlot.FigureBorder = new LineStyle()
+                {
+                    // Set chart border properties
+                    Color = GetDrawingColor(ChartBorderColor),
+                    Width = ChartBorderSize,
+                    Pattern = ChartBorderStyleMap[ChartBorderStyle],
+                };
+            }
+
+            // Set title properties
+            if (Title != null)
+            {
+                myPlot.Title(Title);
+                myPlot.Axes.Title.Label.FontSize = TitleFontSize;
+                myPlot.Axes.Title.Label.ForeColor = GetDrawingColor(TitleFontColor);
+                myPlot.Axes.Title.Label.Bold = TitleFontBold;
+                myPlot.Axes.Title.Label.FontName = FontName;
+            }
+
+            // Set margins settings
+            if (AreaOrientation == Orientations.Horizontal)
+            {
+                myPlot.Axes.Margins(left: AxesMarginsLeft, right: AxesMarginsRight, bottom: AxesMarginsDown, top: AxesMarginsTop);
+            }
+            else
+            {
+                myPlot.Axes.Margins(left: AxesMarginsLeft, right: AxesMarginsRight, bottom: AxesMarginsDown, top: AxesMarginsTop);
+            }
+
+            // Set filepath
+
+            string Filepath = _outputFolderPath ?? Directory.GetCurrentDirectory();
+
+            // Set filename
+            return SaveInFormat(myPlot, width, height, Filepath, filename, Format);
         }
     }
 }
